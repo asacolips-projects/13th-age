53b5085f4f68541d1370d9c657fd5b96a574580e
Matt Smith <matt.smith@mediacurrent.com>
2021-02-20 17:16:13 -0600

    Ranger attacks

--- module/setup/utility-classes.js
+++ module/setup/utility-classes.js
@@ -295,6 +295,7 @@ export class ArchmageUtility {
               let wpnTypes = ['m', 'r', 'j', 'p', 'k'];
               wpnTypes.forEach(wpn => {
                 data.wpn[wpn].die = data.wpn[wpn].dice;
+                data.wpn[wpn].dieNum = data.wpn[wpn].dice.replace('d', '');
                 data.wpn[wpn].dice = data.wpn[wpn].value;
                 data.wpn[wpn].atk = data.wpn[wpn].attack;
                 data.wpn[wpn].dmg = data.wpn[wpn].dmg;
