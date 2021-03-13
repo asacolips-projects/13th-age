def73b5559b969f6a43ae6d20f5d05abc83f1c83
Matt Smith <mattsmith142003@gmail.com>
2021-03-12 20:42:49 -0600

    118: Add proof of concept for powers

--- templates/vue/components/actor/character/main/archmage-actor-c-powers.vue
+++ templates/vue/components/actor/character/main/archmage-actor-c-powers.vue
@@ -1,16 +1,53 @@
 <template>
   <section :class="classes" data-tab="powers">
-    <h2>powers</h2>
+    <section v-for="groupKey in groups" :key="groupKey" class="power-group">
+      <div class="power-group-header flexrow">
+        <h2 class="power-group-title unit-title">{{localize(groupKey)}}</h2>
+        <div class="item-controls">
+          <a class="item-control item-create" data-item-type="power" :data-power-type="groupKey"><i class="fas fa-plus"></i> Add</a>
+        </div>
+      </div>
+      <ul class="power-group-content flexcol">
+        <li v-for="(power, powerKey) in powerGroups[groupKey]" :key="powerKey" class="power flexrow">
+          <h3 class="power-title unit-subtitle">{{power.name}}</h3>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
 <script>
 export default {
   props: ['actor', 'tab'],
-  data: () => ({}),
+  data: function() {
+    return {
+      groups: [
+        'feature',
+        'talent',
+        'power',
+        'spell',
+        'other'
+      ]
+    }
+  },
   computed: {
     classes() {
       return `section section--powers flexcol${this.tab.active ? ' active' : ''}`;
+    },
+    powerGroups() {
+      let powers = this.actor.items.filter(i => i.type == 'power');
+      let powersByGroup = duplicate(powers).reduce((powerGroup, power) => {
+        if (power.data.powerType.value) {
+          let group = power.data.powerType.value ? power.data.powerType.value : 'other';
+          if (!powerGroup[group]) {
+            powerGroup[group] = [];
+          }
+          powerGroup[group].push(power);
+        }
+        return powerGroup;
+      }, {});
+
+      return powersByGroup;
     }
   },
   methods: { /* See created. */},
