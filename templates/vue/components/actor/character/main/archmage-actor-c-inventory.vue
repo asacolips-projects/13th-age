f90414215c2ebb231650a357ff8178274c702501
Matt Smith <mattsmith142003@gmail.com>
2021-03-15 17:38:47 -0500

    118: Start inventory

--- templates/vue/components/actor/character/main/archmage-actor-c-inventory.vue
+++ templates/vue/components/actor/character/main/archmage-actor-c-inventory.vue
@@ -1,6 +1,51 @@
 <template>
   <section :class="classes" data-tab="inventory">
-    <h2>Inventory</h2>
+    <!-- Sorts and filters. -->
+    <header class="equipment-filters flexrow">
+      <div class="sort-equipment">
+        <label for="equipment-sort">{{localize('Sort')}}</label>
+        <select name="equipment-sort" v-model="sortBy">
+          <option v-for="option in sortOptions" :key="option" :value="option.value">{{localize(option.label)}}</option>
+        </select>
+      </div>
+      <div class="filter-search-equipment">
+        <input type="text" name="equipment-filter-search" v-model="searchValue" :placeholder="localize('Filter equipment by name')"/>
+      </div>
+      <div class="import-equipment">
+        <button class="item-import button" title="Create Equipment" data-item-type="equipment" data-type="equipment" type="button"><i class="fas fa-atlas"></i> {{localize('ARCHMAGE.import')}}</button>
+      </div>
+    </header>
+    <!-- Equipment, by group. -->
+    <section class="equipment-group">
+      <div class="equipment-group-header">
+        <!-- Group title and add button. -->
+        <div class="equipment-header-title grid equipment-grid">
+          <h2 class="equipment-group-title unit-title">{{localize('Magic Items')}}</h2>
+          <div class="item-controls">
+            <a class="item-control item-create" data-item-type="equipment"><i class="fas fa-plus"></i> Add</a>
+          </div>
+        </div>
+        <!-- Column labels. -->
+        <div class="equipment-header-labels grid equipment-grid">
+          <div class="equipment-name">{{localize('Equipment Name')}}</div>
+          <div class="item-controls">{{localize('Edit')}}</div>
+        </div>
+      </div>
+      <ul class="equipment-group-content flexcol">
+        <li v-for="(equipment, equipmentKey) in equipment" :key="equipmentKey" :class="concat('equipment-item equipment-item--', equipment._id)" :data-item-id="equipment._id">
+          <!-- Clickable equipment header. -->
+          <div :class="concat('equipment-summary grid equipment-grid ', (equipment.data.equipmentUsage.value ? equipment.data.equipmentUsage.value : 'other'))">
+            <archmage-h-rollable name="item" :hide-icon="true" type="item" :opt="equipment._id"><img :src="equipment.img" class="equipment-image"/></archmage-h-rollable>
+            <a class="equipment-name" v-on:click="toggleEquipment" :data-item-id="equipment._id">
+              <h3 class="equipment-title unit-subtitle"><span v-if="equipment.data.equipmentLevel.value">[{{equipment.data.equipmentLevel.value}}] </span> {{equipment.name}}</h3>
+            </a>
+          <!-- Expanded equipment content. -->
+          <div :class="concat('equipment-content', (activeEquipment[equipment._id] ? ' active' : ''))" :style="getEquipmentStyle(equipment._id)">
+            <archmage-h-equipment :equipment="equipment" :ref="concat('equipment--', equipment._id)"></archmage-h-equipment>
+          </div>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
@@ -13,11 +58,11 @@ export default {
       sortOptions: [
         { value: 'custom', label: 'Custom' },
         { value: 'name', label: 'Name' },
-        { value: 'chakra', label: 'Chakra' }
+        // { value: 'chakra', label: 'Chakra' } // TODO: Add this after fixing the typo in the template.
       ],
       sortBy: 'custom',
       searchValue: null,
-      activePowers: {}
+      activeEquipment: {}
     }
   },
   computed: {
@@ -25,7 +70,82 @@ export default {
       return `section section--inventory flexcol${this.tab.active ? ' active' : ''}`;
     }
   },
-  methods: { /* See created. */},
+  methods: {
+    /**
+     * Clean a equipment name for usage in group keys.
+     */
+    cleanClassName(string) {
+      return string ? string.toLowerCase().replace(/[^a-zA-z\d]/g, '') : '';
+    },
+    /**
+     * Update the `equipment` prop to be equal to a filtered version of the current
+     * equipment items on the actor. Filters by type and search keys.
+     */
+    getEquipment() {
+      let equipment = this.actor.items.filter(i => i.type == 'equipment');
+      if (this.searchValue) {
+        equipment = equipment.filter(i => {
+          let needle = this.cleanClassName(this.searchValue);
+          let haystack = this.cleanClassName(i.name);
+          return haystack.includes(needle);
+        });
+      }
+      if (this.sortBy == 'name') {
+        equipment = equipment.sort((a,b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      equipment.forEach(i => {
+        if (this.activeEquipment[i._id] == undefined) {
+          this.$set(this.activeEquipment, i._id, {value: false});
+          this.activeEquipment[i._id] = false;
+        }
+      });
+      this.equipment = equipment;
+    },
+    /**
+     * Toggle equipment display (click event).
+     */
+    toggleEquipment(event) {
+      let target = event.currentTarget;
+      let dataset = target.dataset;
+      let id = dataset.itemId;
+      if (id) {
+        // Toggle the state if the equipment is currently being tracked.
+        if (this.activeEquipment[id] !== undefined) {
+          this.$set(this.activeEquipment, id, !this.activeEquipment[id]);
+        }
+        // Otherwise, assume it should be open since this was click event.
+        else {
+          this.$set(this.activeEquipment, id, true);
+        }
+      }
+    },
+    /**
+     * Calculate CSS height of equipment.
+     */
+    getEquipmentStyle(equipmentId) {
+      // Retrieve the element from our refs.
+      let equipment = this.$refs[`equipment--${equipmentId}`];
+      let height = 0;
+
+      // Set the height if there's a ref.
+      if (equipment) {
+        height = this.activeEquipment[equipmentId] ? `${equipment[0].$el.clientHeight}px` : `0px`;
+      }
+
+      // Return CSS style object.
+      return {
+        maxHeight: height
+      };
+    }
+  },
   async created() {
     for (let [k,v] of Object.entries(window.archmageVueMethods.methods)) {
       this[k] = v;
---
+++ templates/vue/components/helpers/archmage-h-equipment.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="power">
+    <!-- Group, range, and quick info. -->
+    <header class="power-header flexcol">
+      <strong v-if="power.data.group.value">{{power.data.group.value}}</strong>
+      <strong v-if="power.data.range.value">{{power.data.range.value}}</strong>
+      <div class="power-subheader flexrow">
+        <strong v-if="power.data.actionType.value">{{constants.actionTypes[power.data.actionType.value]}} {{localize('Action')}}</strong>
+        <strong v-if="power.data.powerUsage.value">{{constants.powerUsages[power.data.powerUsage.value]}}</strong>
+        <strong v-if="power.data.powerType.value">{{constants.powerTypes[power.data.powerType.value]}}</strong>
+      </div>
+    </header>
+    <!-- Primary properties (attack, hit, effect, etc.). -->
+    <section class="power-details flexcol">
+      <div class="power-detail" v-for="field in powerDetailFields" :key="field">
+        <strong class="power-detail-label">{{power.data[field].label}}:</strong> <span class="power-detail-value" v-html="power.data[field].value"></span>
+      </div>
+    </section>
+    <!-- Feats. -->
+    <section class="power-feats flexcol">
+      <div v-for="(feat, tier) in power.data.feats" :key="tier" :class="concat('power-feat ', (feat.isActive.value ? 'active' : ''))">
+        <strong class="power-detail-label">{{localize(concat('ARCHMAGE.CHAT.', tier))}}:</strong>
+        <div class="power-detail-content" v-html="feat.description.value"></div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['power'],
+  data: () => ({}),
+  computed: {
+    constants() {
+      return CONFIG.ARCHMAGE;
+    },
+    powerDetailFields() {
+      let powerFields = [
+        'trigger',
+        'target',
+        'always',
+        'attack',
+        'hit',
+        'hitEven',
+        'hitOdd',
+        'crit',
+        'miss',
+        'missEven',
+        'missOdd',
+        'cost',
+        'castBroadEffect',
+        'castPower',
+        'sustainedEffect',
+        'finalVerse',
+        'special',
+        'effect',
+        'spellLevel3',
+        'spellLevel5',
+        'spellLevel7',
+        'spellLevel9',
+        'spellChain',
+        'breathWeapon',
+        'recharge',
+      ];
+
+      powerFields = powerFields.filter(p => this.power.data[p].value);
+
+      return powerFields;
+    }
+  },
+  methods: {},
+  async created() {
+    for (let [k,v] of Object.entries(window.archmageVueMethods.methods)) {
+      this[k] = v;
+    }
+  },
+  async mounted() {}
+}
+</script>
