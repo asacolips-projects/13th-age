86473ba23756f39023be5d0cf63baac8b16fa3a2
Federico Pederzolli <f.pederzolli@vigilidelfuocoarco.it>
2021-04-05 11:08:41 +0200

    Migrations for items, scenes and compendia

--- module/setup/migration.js
+++ module/setup/migration.js
@@ -14,11 +14,9 @@ export const migrateWorld = async function() {
         await a.update(updateData, {enforceTypes: false});
       }
       // Actor Data Cleanup
-      console.log(`Cleaning Actor  ${a.name} data...`);
       cleanActorData(a.data);
       // Call prepareData to recompute dynamic data
       a.prepareData();
-      console.log(a);
     } catch(err) {
       err.message = `Failed system migration for Actor ${a.name}: ${err.message}`;
       console.error(err);
@@ -26,48 +24,46 @@ export const migrateWorld = async function() {
   }
 
   // Migrate World Items
-  // for ( let i of game.items.entities ) {
-    // try {
-      // const updateData = migrateItemData(i.data);
-      // if ( !isObjectEmpty(updateData) ) {
-        // console.log(`Migrating Item entity ${i.name}`);
-        // await i.update(updateData, {enforceTypes: false});
-      // }
-    // } catch(err) {
-      // err.message = `Failed system migration for Item ${i.name}: ${err.message}`;
-      // console.error(err);
-    // }
-  // }
+  for ( let i of game.items.entities ) {
+    try {
+      const updateData = migrateItemData(i.data);
+      if ( !isObjectEmpty(updateData) ) {
+        console.log(`Migrating Item entity ${i.name}`);
+        await i.update(updateData, {enforceTypes: false});
+      }
+      // Item Data Cleanup
+      cleanItemData(i.data);
+    } catch(err) {
+      err.message = `Failed system migration for Item ${i.name}: ${err.message}`;
+      console.error(err);
+    }
+  }
 
   // Migrate Actor Override Tokens
-  // for ( let s of game.scenes.entities ) {
-    // try {
-      // const updateData = migrateSceneData(s.data);
-      // if ( !isObjectEmpty(updateData) ) {
-        // console.log(`Migrating Scene entity ${s.name}`);
-        // await s.update(updateData, {enforceTypes: false});
-      // }
-    // } catch(err) {
-      // err.message = `Failed system migration for Scene ${s.name}: ${err.message}`;
-      // console.error(err);
-    // }
-  // }
+  for ( let s of game.scenes.entities ) {
+    try {
+      const updateData = migrateSceneData(s.data);
+      if ( !isObjectEmpty(updateData) ) {
+        console.log(`Migrating Scene entity ${s.name}`);
+        await s.update(updateData, {enforceTypes: false});
+      }
+    } catch(err) {
+      err.message = `Failed system migration for Scene ${s.name}: ${err.message}`;
+      console.error(err);
+    }
+  }
 
   // Migrate World Compendium Packs
-  // for ( let p of game.packs ) {
-    // if ( p.metadata.package !== "world" ) continue;
-    // if ( !["Actor", "Item", "Scene"].includes(p.metadata.entity) ) continue;
-    // await migrateCompendium(p);
-  // }
+  for ( let p of game.packs ) {
+    if ( p.metadata.package !== "world" ) continue;
+    if ( !["Actor", "Item", "Scene"].includes(p.metadata.entity) ) continue;
+    await migrateCompendium(p);
+  }
 
   // Set the migration as complete
-  // game.settings.set("archmage", "systemMigrationVersion", game.system.data.version);
-  // ui.notifications.info(`System Migration to version ${game.system.data.version} completed!`, {permanent: true});
-
-  ui.notifications.info(`Migration finished. Have fun!`, {permanent: true});
+  game.settings.set("archmage", "systemMigrationVersion", game.system.data.version);
+  ui.notifications.info(`System Migration to version ${game.system.data.version} completed! Have fun!`, {permanent: true});
 };
-
-/* -------------------------------------------- */
 
 /* -------------------------------------------- */
 /*  Entity Type Migration Helpers               */
@@ -79,7 +75,7 @@ export const migrateWorld = async function() {
  * @param {object} actor    The actor data object to update
  * @return {Object}         The updateData to apply
  */
-export const migrateActorData = function(actor) {
+const migrateActorData = function(actor) {
   const updateData = {};
 
   // Actor Data Updates
@@ -125,13 +121,117 @@ function cleanActorData(actorData) {
  * Migrate a single Item entity to incorporate latest data model changes
  * @param item
  */
-export const migrateItemData = function(item) {
+const migrateItemData = function(item) {
   const updateData = {};
   if (item.type == "power") {
     _migrateItemMaxUses(item, updateData);
   }
+  cleanItemData(item.data);
   return updateData;
 };
+
+/* -------------------------------------------- */
+
+/**
+ * Scrub an Item's system data, removing all keys which are not explicitly defined in the system template
+ * @param {Object} itemData    The data object for an Item
+ * @return {Object}             The scrubbed Item data
+ */
+function cleanItemData(itemData) {
+  // Scrub system data
+  const model = game.system.model.Item[itemData.type];
+  itemData.data = filterObject(itemData.data, model);
+
+  // Return the scrubbed data
+  return itemData;
+}
+
+/* -------------------------------------------- */
+
+/**
+ * Migrate a single Scene entity to incorporate changes to the data model of it's actor data overrides
+ * Return an Object of updateData to be applied
+ * @param {Object} scene  The Scene data to Update
+ * @return {Object}       The updateData to apply
+ */
+export const migrateSceneData = function(scene) {
+  const tokens = duplicate(scene.tokens);
+  return {
+    tokens: tokens.map(t => {
+      if (!t.actorId || t.actorLink || !t.actorData.data) {
+        t.actorData = {};
+        return t;
+      }
+      const token = new Token(t);
+      if ( !token.actor ) {
+        t.actorId = null;
+        t.actorData = {};
+      } else if ( !t.actorLink ) {
+        const updateData = migrateActorData(token.data.actorData);
+        t.actorData = mergeObject(token.data.actorData, updateData);
+      }
+      return t;
+    })
+  };
+};
+
+/* -------------------------------------------- */
+
+/**
+ * Apply migration rules to all Entities within a single Compendium pack
+ * @param pack
+ * @return {Promise}
+ */
+export const migrateCompendium = async function(pack) {
+  const entity = pack.metadata.entity;
+  if ( !["Actor", "Item", "Scene"].includes(entity) ) return;
+
+  // Unlock the pack for editing
+  const wasLocked = pack.locked;
+  await pack.configure({locked: false});
+
+  // Begin by requesting server-side data model migration and get the migrated content
+  await pack.migrate();
+  const content = await pack.getContent();
+
+  // Iterate over compendium entries - applying fine-tuned migration functions
+  for ( let ent of content ) {
+    let updateData = {};
+    try {
+      switch (entity) {
+        case "Actor":
+          updateData = migrateActorData(ent.data);
+          break;
+        case "Item":
+          updateData = migrateItemData(ent.data);
+          break;
+        case "Scene":
+          updateData = migrateSceneData(ent.data);
+          break;
+      }
+      if ( isObjectEmpty(updateData) ) continue;
+
+      // Save the entry, if data was changed
+      updateData["_id"] = ent._id;
+      await pack.updateEntity(updateData);
+      console.log(`Migrated ${entity} entity ${ent.name} in Compendium ${pack.collection}`);
+    }
+
+    // Handle migration failures
+    catch(err) {
+      err.message = `Failed system migration for entity ${ent.name} in pack ${pack.collection}: ${err.message}`;
+      console.error(err);
+    }
+  }
+
+  // Apply the original locked status for the pack
+  pack.configure({locked: wasLocked});
+  console.log(`Migrated all ${entity} entities from Compendium ${pack.collection}`);
+};
+
+/* -------------------------------------------- */
+/*  Low level migration utilities
+/* -------------------------------------------- */
 
 function _migrateItemMaxUses(item, updateData) {
   if ( item.data.quantity.value && !item.data.maxQuantity.value ) {
@@ -139,5 +239,3 @@ function _migrateItemMaxUses(item, updateData) {
   }
   return updateData;
 }
-
-/* -------------------------------------------- */
