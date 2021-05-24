01cb8e3d6e07f42230b52bc1f297cd985096c3f4
Matt Smith <mattsmith142003@gmail.com>
2021-05-23 21:37:29 -0500

    Update tour

--- module/tours/tourguide.js
+++ module/tours/tourguide.js
@@ -1,12 +1,13 @@
 import { OneDotSevenTour } from './1_7_tour.js';
 import { OneDotEightTour } from './1_8_tour.js';
 import { OneDotNineTour } from './1_9_tour.js';
+import { OneDotTenTour } from './1_10_tour.js';
 
 export class TourGuide {
 
     startNewFeatureTours() {
         let lastTourVersion = game.settings.get("archmage", "lastTourVersion");
-        
+
         if (isNewerVersion("1.7.0", lastTourVersion)) {
             new OneDotSevenTour().start();
         }
@@ -15,6 +16,9 @@ export class TourGuide {
         }
         else if (isNewerVersion("1.9.0", lastTourVersion)) {
             new OneDotNineTour().start();
+        }
+        else if (isNewerVersion("1.10.0", lastTourVersion)) {
+            new OneDotTenTour().start();
         }
     }
 }
