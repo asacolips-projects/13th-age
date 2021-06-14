e388c5235138a89a6fc7626587f051277740a43e
Neil de Carteret <neil@lumphammer.com>
2021-06-14 12:57:32 +0100

    honor the originalCritDamage setting

--- module/hooks/preCreateChatMessageHandler.mjs
+++ module/hooks/preCreateChatMessageHandler.mjs
@@ -168,13 +168,18 @@ export default class preCreateChatMessageHandler {
                                 // If there's a crit, double the formula and reroll. If there's a
                                 // fail with no crit, 0 it out.
                                 if (has_crit) {
-                                    new_formula = `${roll_data.formula}+${roll_data.formula}`;
+                                    if (game.settings.get('archmage', 'originalCritDamage')) {
+                                        new_formula = `(${roll_data.formula}) * 2`;
+                                    } else {
+                                        new_formula = `${roll_data.formula}+${roll_data.formula}`;
+                                    }
                                     $roll_self.addClass('dc-crit');
                                 }
                                 else {
                                     new_formula = `0`;
                                     $roll_self.addClass('dc-fail');
                                 }
+                                console.log(new_formula);
                                 // Reroll and recalculate.
                                 let new_roll = new Roll(new_formula).roll();
                                 // Update inline roll's markup.
