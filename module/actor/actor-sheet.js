758583f27e923d167a754e2b41dfeee767471f5c
Federico Pederzolli <f.pederzolli@vigilidelfuocoarco.it>
2021-12-15 17:53:49 +0100

    Allow actor sheet rendering - remove flags from v1 sheet

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -1,4 +1,4 @@
-import { ActorSheetFlags } from './actor-flags.js';
+// import { ActorSheetFlags } from './actor-flags.js';
 import { ArchmagePrepopulate } from '../setup/archmage-prepopulate.js';
 import { ItemArchmage } from "../item/item.js";
 
@@ -724,7 +724,7 @@ export class ActorArchmageSheet extends ActorSheet {
    */
   _onConfigureFlags(event) {
     event.preventDefault();
-    new ActorSheetFlags(this.actor).render(true);
+    // new ActorSheetFlags(this.actor).render(true);
   }
 
   /* -------------------------------------------- */
