0b679bba986c8de7f610ebbfddafbb7d1f36c4b4
Federico Pederzolli <f.pederzolli@vigilidelfuocoarco.it>
2022-02-22 17:35:08 +0100

    Code cleanup

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -1170,49 +1170,21 @@ export class ActorArchmage extends Actor {
   /**
    * Override default method to avoid clamping when isBar=true and not
    * using the .value property when not.
-   * Might need to be looked at again in future Foundry versions.
-   *
-   * Handle how changes to a Token attribute bar are applied to the Actor.
-   * This allows for game systems to override this behavior and deploy special logic.
-   * @param {string} attribute    The attribute path
-   * @param {number} value        The target attribute value
-   * @param {boolean} isDelta     Whether the number represents a relative change (true) or an absolute change (false)
-   * @param {boolean} isBar       Whether the new value is part of an attribute bar, or just a direct value
-   * @return {Promise<documents.Actor>}  The updated Actor document
    */
   async modifyTokenAttribute(attribute, value, isDelta=false, isBar=true) {
-    const current = foundry.utils.getProperty(this.data.data, attribute);
-
-    // Determine the updates to make to the actor data
-    let updates;
+    // Handle hps manually for compatibility with our setup
     if ( attribute === "attributes.hp" ) {
-      // Handle manually for compatibility with our setup
       if ( isDelta ) {
+        const current = foundry.utils.getProperty(this.data.data, attribute);
         value = Number(current.value) + value;
         if ( current.value < 0 ) value -= current.value;
       }
-      updates = {[`data.${attribute}.value`]: value};
-    } else if ( isBar ) {
-      if (isDelta) value = Math.clamped(0, Number(current.value) + value, current.max);
-      updates = {[`data.${attribute}.value`]: value};
+      let updates = {[`data.${attribute}.value`]: value};
+      const allowed = Hooks.call("modifyTokenAttribute", {attribute, value, isDelta, isBar}, updates);
+      return allowed !== false ? this.update(updates) : this;
     } else {
-      if ( isDelta ) value = Number(current) + value;
-      updates = {[`data.${attribute}`]: value};
+      super.modifyTokenAttribute(attribute, value, isDelta, isBar);
     }
-
-    /**
-     * A hook event that fires when a token's resource bar attribute has been modified.
-     * @function modifyTokenAttribute
-     * @memberof hookEvents
-     * @param {object} data           An object describing the modification
-     * @param {string} data.attribute The attribute path
-     * @param {number} data.value     The target attribute value
-     * @param {boolean} data.isDelta  Whether the number represents a relative change (true) or an absolute change (false)
-     * @param {boolean} data.isBar    Whether the new value is part of an attribute bar, or just a direct value
-     * @param {objects} updates       The update delta that will be applied to the Token's actor
-     */
-    const allowed = Hooks.call("modifyTokenAttribute", {attribute, value, isDelta, isBar}, updates);
-    return allowed !== false ? this.update(updates) : this;
   }
 
   /**
