f06b13f40bf1dff6b9330f01cd4584cd7db2f3ee
Matt Smith <mattsmith142003@gmail.com>
2022-06-24 14:33:49 -0500

    Update build

--- vite.config.preview.js
+++ vite.config.preview.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '',
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src/vue')}/`
