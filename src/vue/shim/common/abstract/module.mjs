13c956522a8b1c5b8210f78aa8967515861f61d2
Matt Smith <mattsmith142003@gmail.com>
2022-06-24 19:52:00 -0500

    Remove foundry files

--- src/vue/shim/common/abstract/module.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * Abstract data classes which define the interfaces for data schema, document, and embedded document types.
- * @namespace abstract
- */
-
-export {default as DocumentData} from "./data.mjs";
-export {default as Document} from "./document.mjs";
-export {default as DatabaseBackend} from "./backend.mjs";
