7890545c3043a2f00e3897a81b53700039664053
Cody Swendrowski <cody@ironmoosedevelopment.com>
2023-04-19 13:08:11 -0500

    Swap to a dedicated Item Tiers config

--- src/module/setup/config.js
+++ src/module/setup/config.js
@@ -324,6 +324,13 @@ ARCHMAGE.featTiers = {
   'iconic': 'Omega'
 }
 
+ARCHMAGE.itemTiers = {
+  'adventurer': 'Adventurer',
+  'champion': 'Champion',
+  'epic': 'Epic',
+  'iconic': 'Iconic'
+}
+
 ARCHMAGE.numDicePerLevel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 ARCHMAGE.numDicePerLevel2e = [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 16, 20];
 
