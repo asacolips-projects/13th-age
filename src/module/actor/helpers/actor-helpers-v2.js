14f90882cc7b12caee9693f019d68fd0f9ad9b6e
Ben Straub <ben@straub.cc>
2024-02-26 06:07:58 -0800

    i18n

--- src/module/actor/helpers/actor-helpers-v2.js
+++ src/module/actor/helpers/actor-helpers-v2.js
@@ -88,22 +88,22 @@ export class ActorHelpersV2 {
   static _activatePortraitArtContextMenu(app, element) {
     ContextMenu.create(app, element, '.profile-img', [
       {
-        name: 'Show Portrait',
-        icon: '',
+        name: game.i18n.localize('ARCHMAGE.CHARACTER.showPortrait'),
+        icon: '<i class="fa fa-image-portrait"></i>',
         callback: () => {
           new ImagePopout(app.actor.img, {
-            title: "Portrait: " + app.actor.name,
+            title: game.i18n.format('ARCHMAGE.CHARACTER.showPortraitTitle', {name: app.actor.name}),
             shareable: true,
             uuid: app.actor.uuid,
           }).render(true);
         }
       },
       {
-        name: 'Show Token',
-        icon: '',
+        name: game.i18n.localize('ARCHMAGE.CHARACTER.showToken'),
+        icon: '<i class="fas fa-circle-user"></i>',
         callback: () => {
           new ImagePopout(app.actor.prototypeToken.texture.src, {
-            title: "Token: " + app.actor.name,
+            title: game.i18n.format('ARCHMAGE.CHARACTER.showTokenTitle', {name: app.actor.name}),
             shareable: true,
             uuid: app.actor.uuid,
           }).render(true);
