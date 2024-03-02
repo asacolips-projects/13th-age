4377a7563d522bdffcc94744ea4e4d52cd44a474
Matt Smith <mattsmith142003@gmail.com>
2024-03-02 12:45:08 -0600

    Update vite build versions

--- vite.config.preview.mjs
+++ vite.config.preview.mjs
@@ -1,10 +1,12 @@
-import path from 'path'
+import path, { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '',
+  publicDir: './dist',
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src/vue')}/`,
@@ -20,10 +22,11 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
-    outDir: './dist/vue',
+    outDir: './public',
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/vue/index.js'),
-      name: 'v3boilerplateVueComponents',
+      name: 'v3ArchmageVueComponents',
       formats: ['es'], // also supports 'umd'
       fileName: (format) => `components.vue.${format}.js`,
     },
@@ -31,6 +34,9 @@ export default defineConfig({
       external: [
         'vue',
       ],
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
       output: {
         // Provide global variables to use in the UMD build
         // Add external deps here
@@ -39,7 +45,7 @@ export default defineConfig({
         },
         // Map the external dependency to a local copy of Vue 3 esm.
         paths: {
-          vue: `../scripts/lib/vue.esm-browser.prod.js`
+          vue: `./scripts/lib/vue.esm-browser.js`
         },
         assetFileNames: (assetInfo) => {
           if (assetInfo.name == 'style.css')
