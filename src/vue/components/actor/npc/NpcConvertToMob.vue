a94ea9604e57702fd792ae206ab5be213cb63e50
Ben Straub <ben@straub.cc>
2024-04-01 16:35:38 -0700

    I18n

--- src/vue/components/actor/npc/NpcConvertToMob.vue
+++ src/vue/components/actor/npc/NpcConvertToMob.vue
@@ -1,22 +1,25 @@
 <template>
-	<h3>Convert to Mob</h3>
-	<p>Converts this actor to a mob, with more HP and a multi-attack that loses attacks as the HP are drained.</p>
+	<h3>{{ localize('ARCHMAGE.MOOKMOB.title') }}</h3>
+	<p>{{ localize('ARCHMAGE.MOOKMOB.description') }}</p>
 	<section class="section section-mob-convert flexrow">
-		<label><strong>Number of mooks:</strong></label>
+		<label><strong>{{ localize('ARCHMAGE.MOOKMOB.label') }}:</strong></label>
 		<input type="number" min="2" step="1" v-model="mookCount">
-		<button class="button button--confirm" @click="convertToMob" :disabled="mobConversionDisabled" type="button">Convert
-			to Mob</button>
+		<button class="button button--confirm" @click="convertToMob" :disabled="mobConversionDisabled" type="button">
+			{{ localize('ARCHMAGE.MOOKMOB.button') }}
+		</button>
 	</section>
 </template>
 
 <script>
 import { ref } from 'vue';
+import { localize, getActor } from '@/methods/Helpers';
+
 export default {
 	name: 'NpcConvertToMob',
 	props: ['actor'],
 	setup(props) {
 		const mookCount = ref(2)
-		return { mookCount }
+		return { mookCount, localize }
 	},
 }
 </script>
