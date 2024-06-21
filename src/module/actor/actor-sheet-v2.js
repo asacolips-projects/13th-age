526dce1de8bee96e6106b11ebdea68532386c37b
Matt Smith <mattsmith142003@gmail.com>
2024-06-21 17:10:42 -0500

    Bugfix for decreasing AC on Foundry v12
    - Added override for the `_getSubmitData()` method of the actor sheet to
      return a diff of the form data on v12. This appears to be a regression
      with how form data is returned on v12, and so there's a version check
      to allow the existing behavior to keep working on v11.

--- src/module/actor/actor-sheet-v2.js
+++ src/module/actor/actor-sheet-v2.js
@@ -1083,4 +1083,22 @@ export class ActorArchmageSheetV2 extends ActorSheet {
       actor.createEmbeddedDocuments('Item', powers);
     }
   }
+
+  /** @override */
+  _getSubmitData(updateData={}) {
+    // Bugfix for Foundry v12.
+    if (foundry.utils.isNewerVersion(game.version, '12')) {
+      // Retrieve the data from the upstream method.
+      let data = super._getSubmitData(updateData);
+      // Retrieve a copy of the existing actor data.
+      let old = foundry.utils.flattenObject(this.object);
+  
+      // Limit data to just the new data.
+      return foundry.utils.diffObject(old, data);
+    }
+    // Keep old behavior for v11.
+    else {
+      return super._getSubmitData(updateData);
+    }
+  }
 }
