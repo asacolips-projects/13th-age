f0200d9935337ce5014cfa8bd4448bac3b03060d
Ben Straub <ben@straub.cc>
2024-11-28 15:56:37 -0800

    Fixed row height

--- src/vue/components/actor/character/main/CharTriggers.vue
+++ src/vue/components/actor/character/main/CharTriggers.vue
@@ -6,27 +6,31 @@
 			<div>Trigger</div>
 		</header>
 
-		<div v-for="power in powersWithTriggers" :key="power._id"
-			:class="[$style.grid, powerUsageClass(power), powerAvailabilityClass(power)]" class="power-summary">
-			<Rollable name="item" :hide-icon="true" type="item" :opt="power._id">
-				<img :src="power.img" alt="Power icon" class="power-icon" />
-			</Rollable>
-			<a class="power-name" @click="expandedPowers[power._id] = !expandedPowers[power._id]">
-				<h3 class="power-title unit-subtitle" :class="$style.nowrap"> {{ power.name }} </h3>
-			</a>
-			<p style="margin: 0" :class="$style.nowrap">{{ power.system.trigger.value }}</p>
-			<div class="power-content" :class="[expandedPowers[power._id] ? 'active' : '', $style.fullwidth]">
-				<Transition name="slide-fade">
-					<Power v-if="expandedPowers[power._id]" :actor="actor" :power="power" :context="context" :flags="flags"
-						:ref="`power--${power._id}`" />
-				</Transition>
+		<div class="power-group-content flexcol">
+			<div v-for="row in powerRows" :key="row.power._id" class="item power-item" :class="`power-item--${row.power._id}`"
+				:data-item-id="row.power._id" data-document-class="Item" data-draggable="true" draggable="true">
+				<div :class="[$style.grid, ...row.classes]" class="power-summary">
+					<Rollable name="item" :hide-icon="true" type="item" :opt="row.power._id" class="flexrow">
+						<img :src="row.power.img" class="power-icon" />
+					</Rollable>
+					<a class="power-name" @click="row.expanded = !row.expanded">
+						<h3 class="power-title unit-subtitle" :class="$style.nowrap"> {{ row.power.name }} </h3>
+					</a>
+					<p style="margin: 0" :class="$style.nowrap">{{ row.power.system.trigger.value }}</p>
+				</div>
+				<div class="power-content" :class="[$style.fullwidth, row.expanded ? 'active' : '']">
+					<Transition name="slide-fade">
+						<Power v-if="row.expanded" :actor="actor" :power="row.power" :context="context" :flags="flags"
+							:ref="`power--${row.power._id}`" />
+					</Transition>
+				</div>
 			</div>
 		</div>
 	</section>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 import Power from '@/components/parts/Power.vue';
 import Rollable from '@/components/parts/Rollable.vue';
 
@@ -38,7 +42,14 @@ const powersWithTriggers = computed(() =>
 		.filter(x => x.system.trigger?.value)
 )
 
-const expandedPowers = ref({});
+const powerRows = ref([]);
+watch(powersWithTriggers, (newPowers) => {
+	powerRows.value = newPowers.map(power => ({
+		power,
+		expanded: false,
+		classes: [powerUsageClass(power), powerAvailabilityClass(power)]
+	}))
+}, { immediate: true })
 
 /**
  * Compute CSS class to assign based on special usage
@@ -59,7 +70,6 @@ function powerUsageClass(power) {
 function powerAvailabilityClass(power) {
 	return power.system?.quantity?.value === 0 ? 'unavailable' : '';
 }
-
 </script>
 
 <style module lang="scss">
