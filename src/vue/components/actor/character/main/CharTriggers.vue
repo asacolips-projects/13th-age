0837e93366a4a74023c61c6e74af36d2ff9027c7
Ben Straub <ben@straub.cc>
2024-11-28 11:12:18 -0800

    Rough in a grid

--- src/vue/components/actor/character/main/CharTriggers.vue
+++ src/vue/components/actor/character/main/CharTriggers.vue
@@ -1,7 +1,17 @@
 <template>
 	<section class="section section--triggers flexcol">
-		<div v-for="power in powersWithTriggers">
-			{{ power.name }}
+		<header :class="$style.grid" class="power-header-labels">
+			<div></div>
+			<div>Power</div>
+			<div>Trigger</div>
+		</header>
+
+		<div v-for="power in powersWithTriggers" :key="power._id" :class="$style.grid">
+			<Rollable name="item" :hide-icon="true" type="item" :opt="power._id">
+				<img :src="power.img" alt="Power icon" class="power-icon" />
+			</Rollable>
+			<div>{{ power.name }}</div>
+			<div>{{ power.system.trigger.value }}</div>
 		</div>
 	</section>
 </template>
@@ -19,3 +29,11 @@ const powersWithTriggers = computed(() =>
 		.filter(x => x.system.trigger?.value)
 )
 </script>
+
+<style module>
+.grid {
+	display: grid;
+	grid-template-columns: 32px 250px auto;
+	grid-gap: 0.5rem;
+}
+</style>
