ee759f9ca94b47de07630eae0d1062e7dd128b63
Ben Straub <ben@straub.cc>
2024-11-28 10:05:41 -0800

    Filter powers and simple display

--- src/vue/components/actor/character/main/CharTriggers.vue
+++ src/vue/components/actor/character/main/CharTriggers.vue
@@ -1,9 +1,21 @@
 <template>
 	<section class="section section--triggers flexcol">
-
+		<div v-for="power in powersWithTriggers">
+			{{ power.name }}
+		</div>
 	</section>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import Power from '@/components/parts/Power.vue';
+import Rollable from '@/components/parts/Rollable.vue';
 
+const props = defineProps(['actor', 'context', 'tab', 'flags'])
+
+const powersWithTriggers = computed(() =>
+	props.actor.items
+		.filter(x => x.type === 'power')
+		.filter(x => x.system.trigger?.value)
+)
 </script>
