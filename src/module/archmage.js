f9e41e9c9270e7fad19d98a9a81edb599e4cacae
Ben Straub <ben@straub.cc>
2025-06-19 18:26:43 -0700

    Reinstate chat message hook

--- src/module/archmage.js
+++ src/module/archmage.js
@@ -1408,7 +1408,8 @@ function uuidv4() {
 }
 
 
-Hooks.on('renderChatMessage', (chatMessage, html, options) => {
+Hooks.on('renderChatMessageHTML', (chatMessage, rawhtml, options) => {
+  const html = $(rawhtml);
 
   // Override the inline roll click behavior.
   html.find('a.inline-roll').addClass('inline-roll--archmage').removeClass('inline-roll');
