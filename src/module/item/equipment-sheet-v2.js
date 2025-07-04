c33f4beee2657867bcdb7bb5401e919ebbbca523
Ben Straub <ben@straub.cc>
2025-07-04 16:47:12 -0700

    Layout and tabs and preview

--- src/module/item/equipment-sheet-v2.js
+++ src/module/item/equipment-sheet-v2.js
@@ -29,8 +29,8 @@ export class ArchmageEquipmentSheetV2 extends VueRenderingMixin(ArchmageBaseItem
       toggle: this._toggleEffect
     },
     position: {
-      width: 860,
-      height: 630,
+      width: 950,
+      height: 550,
     },
     window: {
       resizable: true,
@@ -88,19 +88,9 @@ export class ArchmageEquipmentSheetV2 extends VueRenderingMixin(ArchmageBaseItem
             label: game.i18n.localize('ARCHMAGE.details'),
             active: true,
           },
-          attack: {
-            key: 'attack',
-            label: game.i18n.localize('ARCHMAGE.attack'),
-            active: false,
-          },
-          special: {
-            key: 'special',
-            label: game.i18n.localize('ARCHMAGE.special'),
-            active: false,
-          },
-          feats: {
-            key: 'feats',
-            label: game.i18n.localize('ARCHMAGE.feats'),
+          bonuses: {
+            key: 'bonuses',
+            label: game.i18n.localize('ARCHMAGE.bonuses'),
             active: false,
           },
           effects: {
@@ -136,19 +126,16 @@ export class ArchmageEquipmentSheetV2 extends VueRenderingMixin(ArchmageBaseItem
 
     // Enrich the description.
     context.editors = {
-      // 'system.description.value': {
-      //   enriched: await wrapRolls(this.item.system.description.value ?? '', [], 'short', {}, 'description', enrichmentOptions),
-      //   element: foundry.applications.elements.HTMLProseMirrorElement.create({
-      //     ...editorOptions,
-      //     name: 'system.description.value',
-      //     value: context.system.description?.value ?? '',
-      //   }),
-      // },
+      'system.description.value': {
+        enriched: await wrapRolls(this.item.system.description.value ?? '', [], 'short', {}, 'description', enrichmentOptions),
+        element: foundry.applications.elements.HTMLProseMirrorElement.create({
+          ...editorOptions,
+          name: 'system.description.value',
+          value: context.system.description?.value ?? '',
+        }),
+      },
     };
 
-    // Enrich powers and feats.
-    // await this._enrichPowers(context, enrichmentOptions, editorOptions);
-    await this._enrichFeats(context, enrichmentOptions, editorOptions);
 
     // Make another pass through the editors to fix the element contents.
     for (let [field, editor] of Object.entries(context.editors)) {
@@ -158,188 +145,5 @@ export class ArchmageEquipmentSheetV2 extends VueRenderingMixin(ArchmageBaseItem
     }
 
     return context;
-  }
-
-  /**
-   * Enrich values for power fields.
-   *
-   * @param {object} context
-   * @param {object} enrichmentOptions
-   * @param {object} editorOptions
-   */
-  async _enrichPowers(context, enrichmentOptions, editorOptions) {
-    // Enrich other fields.
-    const spellFields = CONFIG.ARCHMAGE.is2e ? [
-      'spellLevel2',
-      'spellLevel3',
-      'spellLevel4',
-      'spellLevel5',
-      'spellLevel6',
-      'spellLevel7',
-      'spellLevel8',
-      'spellLevel9',
-      'spellLevel10',
-      'spellLevel11',
-    ] : [
-      'spellLevel3',
-      'spellLevel5',
-      'spellLevel7',
-      'spellLevel9',
-    ];
-    const powerFields = [
-      'trigger',
-      'sustainOn',
-      'target',
-      'always',
-      'attack',
-      'hit',
-      'hitEven',
-      'hitOdd',
-      'crit',
-      'miss',
-      'missEven',
-      'missOdd',
-      'resources',
-      'castBroadEffect',
-      'castPower',
-      'sustainedEffect',
-      'finalVerse',
-      'special',
-      'effect',
-      ...spellFields,
-      'spellChain',
-      'breathWeapon',
-      'recharge',
-    ];
-
-    for (let field of powerFields) {
-      context.editors[field] = {
-        // @todo write a power enricher.
-        enriched: await wrapRolls(this.item.system[field].value ?? '', [], 'short', {}, field, enrichmentOptions),
-        element: foundry.applications.elements.HTMLProseMirrorElement.create({
-          ...editorOptions,
-          name: `system.${field}.value`,
-          value: context.system[field]?.value ?? '',
-        }),
-      };
-    }
-  }
-
-  /**
-   * Enrich values for feats.
-   *
-   * @param {object} context
-   * @param {object} enrichmentOptions
-   * @param {object} editorOptions
-   */
-  async _enrichFeats(context, enrichmentOptions, editorOptions) {
-    // Enrich feats.
-    if (this.item.system.feats) {
-      for (let [featKey, feat] of Object.entries(this.item.system.feats)) {
-        context.editors[`feat.${featKey}`] = {
-          enriched: await wrapRolls(feat.description.value ?? '', [], 'short', {}, featKey, enrichmentOptions),
-          element: foundry.applications.elements.HTMLProseMirrorElement.create({
-            ...editorOptions,
-            name: `system.feats.${featKey}.description.value`,
-            value: feat.description.value ?? '',
-          }),
-        }
-      }
-    }
-  }
-
-  /* ---------------------------------------------------- */
-
-  /**
-   * Add/delete/reorder feats on a power.
-   *
-   * @param {Event} event
-   *   Html event that triggered the method.
-   */
-  static async _updateFeat(event, target) {
-    if (!this.isEditable) return;
-
-    let dataset = target.dataset;
-
-    let item = this.item;
-    if (item.type != "power") return;
-
-    let featIndex = Number(dataset.featKey);
-    let feats = item.system.feats;
-
-    let deleteFeat = (async () => {return;});
-    switch(dataset.action) {
-      case 'createFeat':
-        if (feats) feats = Object.values(feats);
-        else feats = [];
-        feats.push({
-          "description": {
-            "type": "String",
-            "value": ""
-          },
-          "isActive": {
-            "type": "Boolean",
-            "value": false
-          },
-          "tier": {
-            "type": "String",
-            "value": "adventurer"
-          },
-          "powerUsage": {
-            "type": "String",
-            "value": ""
-          },
-          "quantity": {
-            "type": "Number",
-            "value": null
-          },
-          "maxQuantity": {
-            "type": "Number",
-            "value": null
-          }
-        });
-        await item.update({'system.feats': Object.assign({}, feats)});
-        return;
-      case 'deleteFeat':
-        deleteFeat = (async () => {
-          let newFeats = foundry.utils.deepClone(feats);
-          delete newFeats[featIndex];
-          newFeats = Object.assign({}, Object.values(newFeats));  // Re-index from 0
-          let updateData = {'system.feats': newFeats};
-          for (let key of Object.keys(item.system.feats)) {
-            if (!newFeats[key]) updateData[`system.feats.-=${key}`] = null;
-          }
-          await item.update(updateData);
-        });
-        break;
-      case 'moveFeatUp':
-        if (featIndex == 0) return;
-        feats = Object.values(feats);
-        [feats[featIndex], feats[featIndex - 1]] = [feats[featIndex - 1], feats[featIndex]]
-        await item.update({'system.feats': Object.assign({}, feats)});
-        return;
-      case 'moveFeatDown':
-        feats = Object.values(feats);
-        if (featIndex >= feats.length - 1) return;
-        [feats[featIndex + 1], feats[featIndex]] = [feats[featIndex], feats[featIndex + 1]]
-        await item.update({'system.feats': Object.assign({}, feats)});
-        return;
-    }
-
-    let bypass = event.shiftKey ? true : false;
-    if (bypass) {
-      await deleteFeat();
-      return;
-    }
-    foundry.applications.api.DialogV2.prompt({
-      window: {title: 'Delete feat?'},
-      content: `<p>${game.i18n.localize("ARCHMAGE.CHAT.DeleteConfirm")}</p>`,
-      rejectClose: false,
-      ok: {
-        callback: (event, button, dialog) => {
-          deleteFeat();
-        }
-      }
-    });
   }
 }
