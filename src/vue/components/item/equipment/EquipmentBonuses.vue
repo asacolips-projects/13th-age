7feed4dc7500a7a444348d6e1273d986ac751a34
Ben Straub <ben@straub.cc>
2025-07-05 10:10:58 -0700

    Defenses

--- src/vue/components/item/equipment/EquipmentBonuses.vue
+++ src/vue/components/item/equipment/EquipmentBonuses.vue
@@ -37,6 +37,73 @@
         <input type="number" name="system.attributes.attack.arcane.bonus" v-model="item.system.attributes.attack.arcane.bonus" />
       </div>
     </div>
+  </fieldset>
+
+  <fieldset class="fieldset-defense">
+    <legend>{{ game.i18n.localize('ARCHMAGE.ITEM.defenseBonuses') }}</legend>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.acBonus') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.ac.bonus" v-model="item.system.attributes.ac.bonus" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.pdBonus') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.pd.bonus" v-model="item.system.attributes.pd.bonus" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.mdBonus') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.md.bonus" v-model="item.system.attributes.md.bonus" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.hpBonus') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.hp.bonus" v-model="item.system.attributes.hp.bonus" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.recoveriesBonus') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.recoveries.bonus" v-model="item.system.attributes.recoveries.bonus" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.saveBonus') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.save.bonus" v-model="item.system.attributes.save.bonus" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label>
+        {{ game.i18n.localize('ARCHMAGE.ITEM.saveBonusHPTreshold') }}
+      </label>
+      <div class="field">
+        <input type="number" name="system.attributes.save.threshold" v-model="item.system.attributes.save.threshold" />
+      </div>
+    </div>
 
   </fieldset>
 
