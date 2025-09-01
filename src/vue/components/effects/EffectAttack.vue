309e8ecd0388eee06848982b813459977a963b62
Ben Straub <ben@straub.cc>
2025-09-01 16:33:11 -0700

    Remove fieldset wrappers

--- src/vue/components/effects/EffectAttack.vue
+++ src/vue/components/effects/EffectAttack.vue
@@ -1,82 +1,78 @@
 <template>
-	<fieldset class="fieldset-description">
-		<legend>{{ localize('ARCHMAGE.ITEM.attackBonuses') }}</legend>
-
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.toHitBonus') }} </label>
-			<div class="field">
-				<input type="number" v-model="viewModel.attackMod" />
-			</div>
+	<div class="form-group">
+		<label> {{ localize('ARCHMAGE.ITEM.toHitBonus') }} </label>
+		<div class="field">
+			<input type="number" v-model="viewModel.attackMod" />
 		</div>
+	</div>
 
-		<div class="form-group" v-if="!isNpc">
-			<label> {{ localize('ARCHMAGE.ITEM.meleeBonus') }} </label>
-			<div class="field">
-				<input type="number" v-model="viewModel.meleeBonus" />
-			</div>
+	<div class="form-group" v-if="!isNpc">
+		<label> {{ localize('ARCHMAGE.ITEM.meleeBonus') }} </label>
+		<div class="field">
+			<input type="number" v-model="viewModel.meleeBonus" />
 		</div>
+	</div>
 
-		<div class="form-group" v-if="!isNpc">
-			<label> {{ localize('ARCHMAGE.ITEM.rangedBonus') }} </label>
-			<div class="field">
-				<input type="number" v-model="viewModel.rangedBonus" />
-			</div>
+	<div class="form-group" v-if="!isNpc">
+		<label> {{ localize('ARCHMAGE.ITEM.rangedBonus') }} </label>
+		<div class="field">
+			<input type="number" v-model="viewModel.rangedBonus" />
 		</div>
+	</div>
 
-		<div class="form-group" v-if="!isNpc">
-			<label> {{ localize('ARCHMAGE.ITEM.divineBonus') }} </label>
-			<div class="field">
-				<input type="number" v-model="viewModel.divineBonus" />
-			</div>
+	<div class="form-group" v-if="!isNpc">
+		<label> {{ localize('ARCHMAGE.ITEM.divineBonus') }} </label>
+		<div class="field">
+			<input type="number" v-model="viewModel.divineBonus" />
 		</div>
+	</div>
 
-		<div class="form-group" v-if="!isNpc">
-			<label> {{ localize('ARCHMAGE.ITEM.arcaneBonus') }} </label>
-			<div class="field">
-				<input type="number" v-model="viewModel.arcaneBonus" />
-			</div>
+	<div class="form-group" v-if="!isNpc">
+		<label> {{ localize('ARCHMAGE.ITEM.arcaneBonus') }} </label>
+		<div class="field">
+			<input type="number" v-model="viewModel.arcaneBonus" />
 		</div>
+	</div>
 
-		<div class="form-group" v-if="!isNpc">
-			<label>
-				{{ localize('ARCHMAGE.ITEM.meleeWeaponDamageBonus') }}
-				<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.diceExpressionHint')" />
-			</label>
-			<div class="field">
-				<input type="text" v-model="viewModel.meleeDice" placeholder="+2d10" />
-			</div>
+	<div class="form-group" v-if="!isNpc">
+		<label>
+			{{ localize('ARCHMAGE.ITEM.meleeWeaponDamageBonus') }}
+			<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.diceExpressionHint')" />
+		</label>
+		<div class="field">
+			<input type="text" v-model="viewModel.meleeDice" placeholder="+2d10" />
 		</div>
+	</div>
 
-		<div class="form-group" v-if="!isNpc">
-			<label>
-				{{ localize('ARCHMAGE.ITEM.rangedWeaponDamageBonus') }}
-				<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.diceExpressionHint')" />
-			</label>
-			<div class="field">
-				<input type="text" v-model="viewModel.rangedDice" placeholder="+2d10" />
-			</div>
+	<div class="form-group" v-if="!isNpc">
+		<label>
+			{{ localize('ARCHMAGE.ITEM.rangedWeaponDamageBonus') }}
+			<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.diceExpressionHint')" />
+		</label>
+		<div class="field">
+			<input type="text" v-model="viewModel.rangedDice" placeholder="+2d10" />
 		</div>
+	</div>
 
-		<div class="form-group">
-			<label>
-				{{ localize('ARCHMAGE.ITEM.critModBonus') }}
-				<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.critModBonusHint')" />
-			</label>
-			<div class="field">
-				<input type="number" v-model="viewModel.critMod" placeholder="0" />
-			</div>
+	<div class="form-group">
+		<label>
+			{{ localize('ARCHMAGE.ITEM.critModBonus') }}
+			<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.critModBonusHint')" />
+		</label>
+		<div class="field">
+			<input type="number" v-model="viewModel.critMod" placeholder="0" />
 		</div>
+	</div>
 
-		<div class="form-group">
-			<label>
-				{{ localize('ARCHMAGE.ITEM.escalationBlocked') }}
-				<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.escalationBlockedHint')" />
-			</label>
-			<div class="field">
-				<input type="checkbox" v-model="viewModel.edBlocked" />
-			</div>
+	<div class="form-group">
+		<label>
+			{{ localize('ARCHMAGE.ITEM.escalationBlocked') }}
+			<InfoBubble :tooltip="localize('ARCHMAGE.ITEM.escalationBlockedHint')" />
+		</label>
+		<div class="field">
+			<input type="checkbox" v-model="viewModel.edBlocked" />
 		</div>
-	</fieldset>
+	</div>
 </template>
 
 <script setup>
--- src/vue/components/effects/EffectOngoing.vue
+++ src/vue/components/effects/EffectOngoing.vue
@@ -1,28 +1,24 @@
 <template>
-		<fieldset class="fieldset-description">
-		<legend>{{ localize('ARCHMAGE.ITEM.ongoingDamage') }}</legend>
-
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.ongoingDamage') }} </label>
-			<div class="field">
-				<input type="number" v-model="flags.ongoingDamage" />
-			</div>
+	<div class="form-group">
+		<label> {{ localize('ARCHMAGE.ITEM.ongoingDamage') }} </label>
+		<div class="field">
+			<input type="number" v-model="flags.ongoingDamage" />
 		</div>
+	</div>
 
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.damageType') }} </label>
-			<div class="field">
-				<input type="text" v-model="flags.ongoingDamageType" />
-			</div>
+	<div class="form-group">
+		<label> {{ localize('ARCHMAGE.ITEM.damageType') }} </label>
+		<div class="field">
+			<input type="text" v-model="flags.ongoingDamageType" />
 		</div>
+	</div>
 
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.ongoingDamageCrit') }} </label>
-			<div class="field">
-				<input type="checkbox" v-model="flags.ongoingDamageCrit" />
-			</div>
+	<div class="form-group">
+		<label> {{ localize('ARCHMAGE.ITEM.ongoingDamageCrit') }} </label>
+		<div class="field">
+			<input type="checkbox" v-model="flags.ongoingDamageCrit" />
 		</div>
-	</fieldset>
+	</div>
 </template>
 
 <script setup>
