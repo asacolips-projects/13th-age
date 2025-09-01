b42e70580ed3f547f1d4b3371af1c5d1f3f89de7
Ben Straub <ben@straub.cc>
2025-09-01 08:59:57 -0700

    Move all ongoing settings to ongoing tab

--- src/vue/components/effects/EffectDefense.vue
+++ src/vue/components/effects/EffectDefense.vue
@@ -145,31 +145,6 @@
 			</div>
 		</div>
 	</fieldset>
-
-	<fieldset class="fieldset-description">
-		<legend>{{ localize('ARCHMAGE.ITEM.ongoingDamage') }}</legend>
-
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.ongoingDamage') }} </label>
-			<div class="field">
-				<input type="number" v-model="flags.ongoingDamage" />
-			</div>
-		</div>
-
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.damageType') }} </label>
-			<div class="field">
-				<input type="text" v-model="flags.ongoingDamageType" />
-			</div>
-		</div>
-
-		<div class="form-group">
-			<label> {{ localize('ARCHMAGE.ITEM.ongoingDamageCrit') }} </label>
-			<div class="field">
-				<input type="checkbox" v-model="flags.ongoingDamageCrit" />
-			</div>
-		</div>
-	</fieldset>
 </template>
 
 <script setup>
@@ -180,13 +155,6 @@ import { InfoBubble, } from '@/components';
 const props = defineProps(['effect', 'context']);
 const { effect } = props;
 const foundryEffect = inject('itemDocument')
-
-const flags = reactive(effect.flags.archmage || {});
-watch(() => flags, (newValue) => {
-	foundryEffect.setFlag('archmage', 'ongoingDamage', newValue.ongoingDamage);
-	foundryEffect.setFlag('archmage', 'ongoingDamageType', newValue.ongoingDamageType);
-	foundryEffect.setFlag('archmage', 'ongoingDamageCrit', newValue.ongoingDamageCrit);
-}, { deep: true })
 
 const viewModel = reactive({ edBlocked: false });
 
