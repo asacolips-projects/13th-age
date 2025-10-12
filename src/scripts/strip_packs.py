9651e01e954267b9676505f1589f8c3ac7bcf846
Federico Pederzolli <f.pederzolli@vigilidelfuocoarco.it>
2025-10-12 15:58:21 +0200

    Strip stats from list values too

--- src/scripts/strip_packs.py
+++ src/scripts/strip_packs.py
@@ -1,15 +1,16 @@
-import yaml
+import yaml  # pip install pyaml
 import os
 
 from typing import Any, Final
 
 
 ROOT_PATH: Final[str] = "./src/packs/src/"
+# ROOT_PATH: Final[str] = "../packs/src/"
 
 
-def strip_packs(obj: Any):
+def strip_packs(obj: Any) -> Any:
     if not isinstance(obj, dict):
-        return
+        return obj
     for k, v in obj.copy().items():
         if k == "_stats":  # We don't want it
             del obj[k] 
@@ -17,11 +18,16 @@ def strip_packs(obj: Any):
             for i in obj[k].copy().keys():
                 if i != "default":
                     del obj[k][i]
-        else:
-            strip_packs(obj[k])
-    
+        elif isinstance(v, dict):
+            obj[k] = strip_packs(v)
+        elif isinstance(v, list):
+            for i, vv in enumerate(v):
+                obj[k][i] = strip_packs(vv)
+    return obj
+
 
 print("Usage: python src/scripts/strip_packs.py\n")
+
 for path in os.listdir(ROOT_PATH):
     path = ROOT_PATH + path
     if os.path.isfile(path):
@@ -34,8 +40,8 @@ for path in os.listdir(ROOT_PATH):
                 obj = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
-        strip_packs(obj)
+        obj = strip_packs(obj)
         with open(filepath, "w") as stream:
             yaml.dump(obj, stream, default_flow_style=False)
             
-print("\nDone.")            
+print("\nDone.")
