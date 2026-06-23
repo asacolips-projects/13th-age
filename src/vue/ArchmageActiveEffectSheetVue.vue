5823a4019744e9bf7e2aa9cd18eb0cd10f82a455
Federico Pederzolli <f.pederzolli@vigilidelfuocoarco.it>
2026-06-23 20:55:25 +0200

    AE vue sheet mode->type for v14

--- src/vue/ArchmageActiveEffectSheetVue.vue
+++ src/vue/ArchmageActiveEffectSheetVue.vue
@@ -201,7 +201,7 @@ watch(viewModel, async (newModel) => {
 		newChanges.push({
 			key: fKey,
 			value: value,
-			mode: "add"
+			type: "add"
 		})
 
 		// melee.dice also applies to monk weapons
@@ -210,7 +210,7 @@ watch(viewModel, async (newModel) => {
 				newChanges.push({
 					key: fKey.replace("melee", k),
 					value: value,
-					mode: "add"
+					type: "add"
 				});
 			});
 		}
@@ -220,7 +220,7 @@ watch(viewModel, async (newModel) => {
 	if (newModel.edBlocked) {
 		newChanges.push({
 			key: 'system.attributes.escalation.value',
-			mode: "override",
+			type: "override",
 			value: '0'
 		});
 	}
